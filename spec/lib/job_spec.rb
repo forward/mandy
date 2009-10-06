6ff4253e8b73730c3879e84be3267ed160474399
trafficbroker <technology@trafficbroker.co.uk>
2009-10-06 14:10:53 +0100

    Revert "Don't need to specify output format"
    This reverts commit 4a1af903404e1f6f66546aa236efc5c7467b6364.

--- spec/lib/job_spec.rb
+++ spec/lib/job_spec.rb
@@ -90,6 +90,7 @@ describe Mandy::Job do
       job = Mandy::Job.new("lola") do
         serialize Mandy::Serializers::Json
         input_format :plain
+        output_format :pants
 
         map do|k,v|
           emit(k, v.split(","))
